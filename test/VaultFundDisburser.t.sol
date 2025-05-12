238c54ae58615afddda0e89307beed5827f4cc77
Ermyas Abebe <ermyast@gmail.com>
2025-05-12 15:26:06 +1000

    Add placeholder test

---
+++ test/VaultFundDisburser.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache 2.0
+pragma solidity 0.8.27;
+
+import {Test} from "forge-std/Test.sol";
+
+contract VaultFundDisburserTest is Test {
+    function setup() public {
+        // TODO: Setup code here
+    }
+
+    function test_RevertIf_InvalidAssetID() public {
+        // TODO: Test code here
+    }
+}
