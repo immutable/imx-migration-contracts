5bd3912c2dbbd9289a4ad9206d0b9be17aaaa03a
Ermyas Abebe <ermyast@gmail.com>
2025-06-02 15:56:38 +1000

    Improve AssetRegistry unit tests

--- test/unit/assets/AssetsRegistry.t.sol
+++ test/unit/assets/AssetsRegistry.t.sol
@@ -4,128 +4,153 @@ pragma solidity ^0.8.27;
 import "@src/assets/AssetMappingRegistry.sol";
 import "forge-std/Test.sol";
 
-contract TestAssetsRegistry is AssetMappingRegistry {
-    function registerAsset(AssetMappingRegistry.AssetDetails memory assetInfo) public {
+contract MockAssetsRegistry is AssetMappingRegistry {
+    function registerAssetMapping(AssetMappingRegistry.AssetDetails memory assetInfo) public {
         _registerAssetMapping(assetInfo);
     }
 
-    function registerAssets(AssetMappingRegistry.AssetDetails[] memory assetInfos) public {
+    function registerAssetMapping(AssetMappingRegistry.AssetDetails[] memory assetInfos) public {
         _registerAssetMappings(assetInfos);
     }
 }
 
-contract AssetsRegistryTest is Test {
-    TestAssetsRegistry public registry;
+contract AssetsMappingRegistryTest is Test {
+    MockAssetsRegistry public mockRegistry;
 
     uint256 public constant TEST_ASSET_ID = 1;
     address public constant TEST_ASSET_ADDRESS = address(0xBEEF);
     uint256 public constant TEST_QUANTUM = 1e11;
 
     function setUp() public {
-        registry = new TestAssetsRegistry();
+        mockRegistry = new MockAssetsRegistry();
     }
 
-    function test_RegisterAsset() public {
-        AssetMappingRegistry.ImmutableXAsset memory imxAsset =
-            AssetMappingRegistry.ImmutableXAsset({id: TEST_ASSET_ID, quantum: TEST_QUANTUM});
+    function test_RegisterAssetMapping() public {
         AssetMappingRegistry.AssetDetails memory assetInfo =
-            AssetMappingRegistry.AssetDetails({assetOnIMX: imxAsset, assetOnZKEVM: TEST_ASSET_ADDRESS});
+            _createAssetDetails(TEST_ASSET_ID, TEST_QUANTUM, TEST_ASSET_ADDRESS);
+
+        assertEq(mockRegistry.getMappedAssetAddress(TEST_ASSET_ID), address(0));
+        assertEq(mockRegistry.getAssetQuantum(TEST_ASSET_ID), 0);
+        assertFalse(mockRegistry.isMapped(TEST_ASSET_ID));
 
         vm.expectEmit(true, true, true, true);
         emit AssetMappingRegistry.AssetMapped(TEST_ASSET_ID, TEST_QUANTUM, TEST_ASSET_ADDRESS);
-        registry.registerAsset(assetInfo);
+        mockRegistry.registerAssetMapping(assetInfo);
 
-        assertEq(registry.getMappedAssetAddress(TEST_ASSET_ID), TEST_ASSET_ADDRESS);
-        assertEq(registry.getAssetQuantum(TEST_ASSET_ID), TEST_QUANTUM);
-        assertTrue(registry.isMapped(TEST_ASSET_ID));
+        assertEq(mockRegistry.getMappedAssetAddress(TEST_ASSET_ID), TEST_ASSET_ADDRESS);
+        assertEq(mockRegistry.getAssetQuantum(TEST_ASSET_ID), TEST_QUANTUM);
+        assertTrue(mockRegistry.isMapped(TEST_ASSET_ID));
     }
 
-    function test_RegisterAssets() public {
+    function test_RegisterAssetMappings() public {
         AssetMappingRegistry.AssetDetails[] memory assetInfos = new AssetMappingRegistry.AssetDetails[](2);
 
-        assetInfos[0] =
-            AssetMappingRegistry.AssetDetails(AssetMappingRegistry.ImmutableXAsset(1, 1e18), address(0xBEEF));
-        assetInfos[1] = AssetMappingRegistry.AssetDetails(AssetMappingRegistry.ImmutableXAsset(2, 1e6), address(0xCAFE));
+        assetInfos[0] = _createAssetDetails(1, 1e18, address(0xBEEF));
+        assetInfos[1] = _createAssetDetails(2, 1e6, address(0xCAFE));
 
         vm.expectEmit(true, true, true, true);
         emit AssetMappingRegistry.AssetMapped(1, 1e18, address(0xBEEF));
         vm.expectEmit(true, true, true, true);
         emit AssetMappingRegistry.AssetMapped(2, 1e6, address(0xCAFE));
 
-        registry.registerAssets(assetInfos);
+        mockRegistry.registerAssetMapping(assetInfos);
 
-        assertEq(registry.getMappedAssetAddress(1), address(0xBEEF));
-        assertEq(registry.getAssetQuantum(1), 1e18);
-        assertEq(registry.getMappedAssetAddress(2), address(0xCAFE));
-        assertEq(registry.getAssetQuantum(2), 1e6);
+        assertEq(mockRegistry.getMappedAssetAddress(1), address(0xBEEF));
+        assertEq(mockRegistry.getAssetQuantum(1), 1e18);
+        assertTrue(mockRegistry.isMapped(1));
+
+        assertEq(mockRegistry.getMappedAssetAddress(2), address(0xCAFE));
+        assertEq(mockRegistry.getAssetQuantum(2), 1e6);
+        assertTrue(mockRegistry.isMapped(2));
     }
 
-    function test_RegisterAsset_ZeroAssetId() public {
-        AssetMappingRegistry.AssetDetails memory assetInfo =
-            AssetMappingRegistry.AssetDetails(AssetMappingRegistry.ImmutableXAsset(0, TEST_QUANTUM), TEST_ASSET_ADDRESS);
+    function test_RevertIf_RegisterAsset_ZeroAssetId() public {
+        AssetMappingRegistry.AssetDetails memory assetInfo = _createAssetDetails(0, TEST_QUANTUM, TEST_ASSET_ADDRESS);
 
         vm.expectRevert(
             abi.encodeWithSelector(AssetMappingRegistry.InvalidAssetDetails.selector, "Asset ID cannot be zero")
         );
-        registry.registerAsset(assetInfo);
+        mockRegistry.registerAssetMapping(assetInfo);
     }
 
-    function test_RegisterAsset_ZeroAddress() public {
-        AssetMappingRegistry.AssetDetails memory assetInfo = AssetMappingRegistry.AssetDetails(
-            AssetMappingRegistry.ImmutableXAsset(TEST_ASSET_ID, TEST_QUANTUM), address(0)
-        );
-
-        vm.expectRevert(
-            abi.encodeWithSelector(AssetMappingRegistry.InvalidAssetDetails.selector, "Asset address cannot be zero")
-        );
-        registry.registerAsset(assetInfo);
-    }
-
-    function test_RegisterAsset_ZeroQuantum() public {
-        AssetMappingRegistry.AssetDetails memory assetInfo = AssetMappingRegistry.AssetDetails(
-            AssetMappingRegistry.ImmutableXAsset(TEST_ASSET_ID, 0), TEST_ASSET_ADDRESS
-        );
+    function test_RevertIf_RegisterAsset_ZeroQuantum() public {
+        AssetMappingRegistry.AssetDetails memory assetInfo = _createAssetDetails(TEST_ASSET_ID, 0, TEST_ASSET_ADDRESS);
 
         vm.expectRevert(
             abi.encodeWithSelector(AssetMappingRegistry.InvalidAssetDetails.selector, "Quantum cannot be zero")
         );
-        registry.registerAsset(assetInfo);
+        mockRegistry.registerAssetMapping(assetInfo);
     }
 
-    function test_RegisterAsset_DuplicateAssetId() public {
-        AssetMappingRegistry.AssetDetails memory assetInfo = AssetMappingRegistry.AssetDetails(
-            AssetMappingRegistry.ImmutableXAsset(TEST_ASSET_ID, TEST_QUANTUM), TEST_ASSET_ADDRESS
-        );
+    function test_RevertIf_RegisterAsset_QuantumAboveBounds() public {
+        AssetMappingRegistry.AssetDetails memory assetInfo =
+            _createAssetDetails(TEST_ASSET_ID, mockRegistry.QUANTUM_UPPER_BOUND(), TEST_ASSET_ADDRESS);
 
-        registry.registerAsset(assetInfo);
+        vm.expectRevert(
+            abi.encodeWithSelector(AssetMappingRegistry.InvalidAssetDetails.selector, "Quantum exceeds upper bound")
+        );
+        mockRegistry.registerAssetMapping(assetInfo);
+    }
+
+    function test_RevertIf_RegisterAsset_ZeroAddress() public {
+        AssetMappingRegistry.AssetDetails memory assetInfo =
+            _createAssetDetails(TEST_ASSET_ID, TEST_QUANTUM, address(0));
+
+        vm.expectRevert(
+            abi.encodeWithSelector(AssetMappingRegistry.InvalidAssetDetails.selector, "Asset address cannot be zero")
+        );
+        mockRegistry.registerAssetMapping(assetInfo);
+    }
+
+    function test_RevertIf_RegisterAsset_AlreadyRegistered() public {
+        AssetMappingRegistry.AssetDetails memory assetInfo =
+            _createAssetDetails(TEST_ASSET_ID, TEST_QUANTUM, TEST_ASSET_ADDRESS);
+
+        mockRegistry.registerAssetMapping(assetInfo);
 
         vm.expectRevert(
             abi.encodeWithSelector(AssetMappingRegistry.InvalidAssetDetails.selector, "Asset already registered")
         );
-        registry.registerAsset(assetInfo);
+        mockRegistry.registerAssetMapping(assetInfo);
     }
 
-    function test_RegisterAssets_EmptyArrays() public {
+    function test_RevertIf_RegisterAssets_EmptyArray() public {
         AssetMappingRegistry.AssetDetails[] memory assetInfos = new AssetMappingRegistry.AssetDetails[](0);
 
         vm.expectRevert(
             abi.encodeWithSelector(AssetMappingRegistry.InvalidAssetDetails.selector, "No assets to register")
         );
-        registry.registerAssets(assetInfos);
+        mockRegistry.registerAssetMapping(assetInfos);
     }
 
-    function test_IsNativeAsset() public {
-        AssetMappingRegistry.AssetDetails memory assetInfo = AssetMappingRegistry.AssetDetails(
-            AssetMappingRegistry.ImmutableXAsset(TEST_ASSET_ID, TEST_QUANTUM), registry.NATIVE_IMX_ADDRESS()
-        );
+    function test_RegisterAsset_Native() public {
+        AssetMappingRegistry.AssetDetails memory nativeAsset =
+            _createAssetDetails(TEST_ASSET_ID, TEST_QUANTUM, mockRegistry.NATIVE_IMX_ADDRESS());
+        AssetMappingRegistry.AssetDetails memory nonNativeAsset =
+            _createAssetDetails(TEST_ASSET_ID + 1, TEST_QUANTUM, TEST_ASSET_ADDRESS);
 
-        registry.registerAsset(assetInfo);
-        assertTrue(registry.isMappedToNativeAsset(TEST_ASSET_ID));
+        // register both assets
+        mockRegistry.registerAssetMapping(nativeAsset);
+        mockRegistry.registerAssetMapping(nonNativeAsset);
 
-        assetInfo =
-            AssetMappingRegistry.AssetDetails(AssetMappingRegistry.ImmutableXAsset(2, TEST_QUANTUM), TEST_ASSET_ADDRESS);
+        assertTrue(mockRegistry.isMappedToNativeAsset(TEST_ASSET_ID));
+        assertFalse(mockRegistry.isMappedToNativeAsset(TEST_ASSET_ID + 1));
+    }
 
-        registry.registerAsset(assetInfo);
-        assertFalse(registry.isMappedToNativeAsset(2));
+    // Helper functions to create structs more concisely
+    function _createImmutableXAsset(uint256 id, uint256 quantum)
+        private
+        pure
+        returns (AssetMappingRegistry.ImmutableXAsset memory)
+    {
+        return AssetMappingRegistry.ImmutableXAsset(id, quantum);
+    }
+
+    function _createAssetDetails(uint256 id, uint256 quantum, address zkAddress)
+        private
+        pure
+        returns (AssetMappingRegistry.AssetDetails memory)
+    {
+        return AssetMappingRegistry.AssetDetails(_createImmutableXAsset(id, quantum), zkAddress);
     }
 }
